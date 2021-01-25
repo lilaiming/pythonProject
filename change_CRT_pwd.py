06d6a78d74c98378b038983532f48a7621c1a4e7
lilaiming <441314423@qq.com>
2021-01-25 10:21:52 +0800

    111

--- change_CRT_pwd.py
+++ change_CRT_pwd.py
@@ -6,7 +6,7 @@ def modify_bacth(path, pwd, username):
 
     for filename in files:
         # pass_name = [
-        # "ASA-Tenant-01.ini",    #排除不需要改密码的文件
+        # "ASA-Tenant-01.ini",    #排除不需要改密码的文件2
         # ]
         # if filename in pass_name:
         #     continue
